e827a88a11f3d8fbddc111d098ac7dbb838f6ff0
Teague Cole <tnc0419@icloud.com>
2022-03-13 14:38:26 -0400

    learning how to use NavigationLink

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,29 +7,18 @@
 
 import SwiftUI
 
-struct CustomText: View {
-    let text: String
-    
-    var body: some View {
-        Text(text)
-    }
-    
-    init(_ text: String) {
-        print("Creating a new custom text")
-        self.text = text
-    }
- }
 
 struct ContentView: View {
     var body: some View {
-        ScrollView(.horizontal) {
-            LazyHStack(spacing: 10) {
-                ForEach(0..<100) {
-                    CustomText("Item \($0)")
-                        .font(.title)
+        NavigationView {
+            List(0..<100) { row in
+                NavigationLink {
+                    Text("Detail \(row)")
+                } label: {
+                    Text("Row \(row)")
                 }
             }
-            .frame(maxWidth: .infinity)
+            .navigationTitle("SwiftUI")
         }
     }
 }
