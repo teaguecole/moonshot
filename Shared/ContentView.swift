2ee3e24f4f0d48f348a994d3680eaefddcf0068a
Teague Cole <tnc0419@icloud.com>
2022-03-13 14:22:29 -0400

    learning about Geometry Reader

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        GeometryReader { geo in
+            Image("Example")
+                .resizable()
+                .scaledToFit()
+                .frame(width: geo.size.width * 0.8)
+                .frame(width: geo.size.width, height: geo.size.height)
+        }
+        
     }
 }
 
