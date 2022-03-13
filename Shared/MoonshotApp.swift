afbe3362a622b047aae6aa9e07ea5a6d508beb5a
Teague Cole <tnc0419@icloud.com>
2022-03-13 14:12:38 -0400

    Initial Commit

---
+++ Shared/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Shared
+//
+//  Created by Teague Cole on 3/13/22.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
